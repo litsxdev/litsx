363c93c90658a7c191cc4a6dd62d5f18d9052878
Rafa Bernad <rafabernad@outlook.com>
2026-05-11 09:18:15 +0200

    fix(ssr): gate module id metadata behind ssr mode

--- test/babel-preset-litsx.test.js
+++ test/babel-preset-litsx.test.js
@@ -960,7 +960,7 @@ describe("@litsx/babel-preset-litsx", () => {
         configFile: false,
         babelrc: false,
         filename: "/virtual/TypedForm.tsx",
-        presets: [[nativePreset, {}]],
+        presets: [[nativePreset, { ssr: true }]],
       }
     );
 
