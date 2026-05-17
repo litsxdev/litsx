74b934b50172fd24731ada69036798f18b8a3123
Rafa Bernad <rafabernad@outlook.com>
2026-05-17 17:11:39 +0200

    refactor(ssr): simplify SSR controller selection

--- packages/core/src/ssr-runtime.js
+++
@@ -1,8 +0,0 @@
-import { registerSsrEffectsController } from "./runtime-controller.js";
-import { SsrEffectsController } from "./ssr-effects-controller.js";
-
-registerSsrEffectsController(
-  (host, ssrContext) => new SsrEffectsController(host, ssrContext),
-);
-
-export { SsrEffectsController };
