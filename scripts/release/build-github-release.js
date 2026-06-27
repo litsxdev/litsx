c742d67b7d997d6639deb5367ab1b410a184836b
Rafa Bernad <rafabernad@outlook.com>
2026-06-27 23:28:52 +0200

    ci: number daily GitHub releases

--- scripts/release/build-github-release.js
+++ scripts/release/build-github-release.js
@@ -148,6 +148,7 @@ const releaseData = {
   tagName: `release-${shortSha}`,
   targetCommitish: releaseCommitSha,
   name: `LitSX release ${releaseCommitDate}`,
+  releaseDate: releaseCommitDate,
   body: buildReleaseBody(changes),
   commitSha: releaseCommitSha,
   packages: changes.map(({ name, version, packageDir, tagName }) => ({
